b99330fccc0c9ead6c52a8b61adc787c1ef602c3
Tjudice <tjudice@umich.edu>
2021-08-05 16:16:07 -0400

    Trying a bunch of things at this point hoping I figure out something

--- packages/protocol/scripts/bash/deploy.sh
+++ packages/protocol/scripts/bash/deploy.sh
@@ -45,7 +45,9 @@ export NETWORK_OPTION="--$NETWORK"
 docker run --name celo-lightestnode -d --restart unless-stopped --stop-timeout 300 -p 127.0.0.1:8545:8545 -p 127.0.0.1:8546:8546 -p 30303:30303 -p 30303:30303/udp -v $PWD:/root/.celo $CELO_IMAGE --verbosity 3 --syncmode lightest --rpc --rpcaddr 0.0.0.0 --rpcapi eth,net,web3,debug,admin,personal --light.serve 90 --light.maxpeers 1000 --maxpeers 1100 --nousb $NETWORK_OPTION --datadir /root/.celo
 echo -n 'Trevor' > secret.txt
 geth account new --password secret.txt
-mv ../UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886 ../.ethereum/keystore
+mv ../UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886 ../../../../../.ethereum/keystore
+docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "ls"
+docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "pwd"
 docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "geth account new --password secret.txt"
 docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "geth account import --datadir '~/home/runner/.ethereum' --password secret.txt UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886"
 docker info
