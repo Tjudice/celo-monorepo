80245c96cd49987130d69ce73c6a6fd71ed99130
Tjudice <tjudice@umich.edu>
2021-08-04 20:02:01 -0400

    Trying to create eth account

--- packages/protocol/scripts/bash/deploy.sh
+++ packages/protocol/scripts/bash/deploy.sh
@@ -30,20 +30,20 @@ sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get install ethereum
 
-geth account new
 
 export CELO_IMAGE=us.gcr.io/celo-org/geth:$NETWORK
 docker pull $CELO_IMAGE
 mkdir celo-data-dir
-# mv UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886 celo-data-dir:/root/.celo/keystore
+mv UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886 ~/.celo/keystore
 # cd celo-data-dir:/root/.celo
 
 cd celo-data-dir
 DATA_DIR="/root/.celo"
 export NETWORK_OPTION="--$NETWORK"
 docker run --name celo-lightestnode -d --restart unless-stopped --stop-timeout 300 -p 127.0.0.1:8545:8545 -p 127.0.0.1:8546:8546 -p 30303:30303 -p 30303:30303/udp -v $PWD:/root/.celo $CELO_IMAGE --verbosity 3 --syncmode lightest --rpc --rpcaddr 0.0.0.0 --rpcapi eth,net,web3,debug,admin,personal --light.serve 90 --light.maxpeers 1000 --maxpeers 1100 --nousb $NETWORK_OPTION --datadir /root/.celo
-mv UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886 $DATA_DIR/keystore
-docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "geth account import --password $DATA_DIR/account/accountSecret ./UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886"
+echo -n 'Trevor' > secret.txt
+geth account new --password secret.txt
+docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "geth account import --password secret.txt ./UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886"
 docker info
 ls
 pwd
