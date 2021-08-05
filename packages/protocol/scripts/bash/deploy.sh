850189343a4f7429e29c1fd377f0a03474c4ded8
Tjudice <tjudice@umich.edu>
2021-08-05 17:38:02 -0400

    Attempting docker fix

--- packages/protocol/scripts/bash/deploy.sh
+++ packages/protocol/scripts/bash/deploy.sh
@@ -38,18 +38,23 @@ docker pull $CELO_IMAGE
 mkdir celo-data-dir
 # cd celo-data-dir:/root/.celo
 ls -a
-mv UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886 .ethereum/keystore
+# mv UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886 .ethereum/keystore
 cd celo-data-dir
-DATA_DIR="/root/.celo"
+DATA_DIR=/root/.celo
 export NETWORK_OPTION="--$NETWORK"
 docker run --name celo-lightestnode -d --restart unless-stopped --stop-timeout 300 -p 127.0.0.1:8545:8545 -p 127.0.0.1:8546:8546 -p 30303:30303 -p 30303:30303/udp -v $PWD:/root/.celo $CELO_IMAGE --verbosity 3 --syncmode lightest --rpc --rpcaddr 0.0.0.0 --rpcapi eth,net,web3,debug,admin,personal --light.serve 90 --light.maxpeers 1000 --maxpeers 1100 --nousb $NETWORK_OPTION --datadir /root/.celo
-echo -n 'Trevor' > secret.txt
-geth account new --password secret.txt
-mv ../UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886 ../../../../../.ethereum/keystore
-docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "ls"
-docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "pwd"
+# echo -n 'Trevor' > secret.txt
+# mv secret.txt $DATA_DIR
+# geth account new --password secret.txt
+# mv ../UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886 ~/.ethereum/keystore
+CONT_ID=$(docker inspect --format="{{.Id}}" celo-lightestnode)
+docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "cd root"
+docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "ls -a"
+docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "cd .."
+docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "echo -n 'Trevor' > secret.txt"
 docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "geth account new --password secret.txt"
-docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "geth account import --datadir '~/home/runner/.ethereum' --password secret.txt UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886"
+docker cp ../UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886 CONT_ID:/root/.celo/keystore
+docker run --rm --net=host -v $DATA_DIR:$DATA_DIR --entrypoint /bin/sh -i $CELO_IMAGE -c "geth account import --datadir 'root/.celo/' --password secret.txt UTC--2021-08-04T04-11-41.231375000Z--7c3ed16519eeceb354ac2d88a6a6722a3a9eb886"
 docker info
 ls
 pwd
