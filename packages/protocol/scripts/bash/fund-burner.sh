811f95d26d3262f427662d8518c17ee161fbf1d2
Tjudice <tjudice@umich.edu>
2021-08-16 00:19:06 -0400

    Fixing truffle incorrect networkid

--- packages/protocol/scripts/bash/fund-burner.sh
+++ packages/protocol/scripts/bash/fund-burner.sh
@@ -4,22 +4,28 @@ set -euo pipefail
 # Funds a burner account with CELO on a specific network
 #
 # Flags:
+# -n: Network to fund account on
 # -k: Private key of address to send funds from
 # -a: Amount of Celo to send to burner (optional)
 
+
+NETWORK=""
 KEY=""
 AMOUNT=""
 
 while getopts 'k:a:' flag; do
   case "${flag}" in
+    n) NETWORK = "${OPTARG}" ;;
     k) KEY="${OPTARG}" ;;
     a) AMOUNT="--amount ${OPTARG}" ;;
     *) error "Unexpected option ${flag}" ;;
   esac
 done
 
+[ -z "$NETWORK" ] && echo "Need to set the NETWORK via the -n flag" && exit 1;
 [ -z "$KEY" ] && echo "Need to set the KEY via the -k flag" && exit 1;
 
 yarn run truffle exec ./scripts/truffle/fund_burner_account.js \
+  --network $NETWORK \
   --key $KEY \
   $AMOUNT
--- packages/protocol/scripts/truffle/fund_burner_account.ts
+++ packages/protocol/scripts/truffle/fund_burner_account.ts
@@ -3,17 +3,13 @@ import { LocalWallet } from '@celo/wallet-local'
 import Web3 from 'web3'
 
 /*
- * A script that reads a backwards compatibility report, deploys changed contracts, and creates
- * a corresponding JSON file to be proposed with `celocli governance:propose`
+ * A script that funds a burner account used to deploy release contracts with CELO on a given network
  *
- * Expects the following flags:
- *   report: The filepath of the backwards compatibility report
- *   network: The network for which artifacts should be
  *
  * Run using truffle exec, e.g.:
- * truffle exec scripts/truffle/make-release \
- *   --network alfajores --build_directory build/alfajores/ --report report.json \
- *   --initialize_data initialize_data.json --proposal proposal.json
+ * truffle exec scripts/truffle/fund_burner_accounts \
+ *   --network alfajores --key 0x0000000000000000000000000000000000000000000000000000000000000000
+ *   --amount 1000000000000000000
  */
 
 module.exports = async (callback: (error?: any) => number) => {
@@ -23,8 +19,6 @@ module.exports = async (callback: (error?: any) => number) => {
     })
 
     const web: Web3 = new Web3('http://127.0.0.1:8545')
-
-    web.setProvider(web3.currentProvider)
 
     // web3.eth.accounts.privateKeyToAccount(
     //   '0x3262cbe4bdd55a27ba11ca4674fc91afe0539f850f3074dc06928c5bf9a0e10d'
@@ -62,10 +56,16 @@ module.exports = async (callback: (error?: any) => number) => {
     //   { to: burnerAddress, amount: '1000000000000000000' }
     // )
 
+    var amount: string = '1000000000000000000'
+
+    if (argv.amount) {
+      amount = argv.amount
+    }
+
     await connection.sendTransaction({
       from: '0x7F871c887e6a430D3c1F434737F568B07559F9E7',
       to: burnerAddress,
-      value: '1000000000000000000',
+      value: amount,
     })
 
     web.eth.personal.defaultAccount = burnerAddress
