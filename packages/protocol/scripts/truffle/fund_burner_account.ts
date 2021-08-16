3f13cd95d4703c9a4eda8b06c23e47a77ed5cdf9
Tjudice <tjudice@umich.edu>
2021-08-16 17:34:36 -0400

    Cleaning up code and adding verify-release call

--- packages/protocol/scripts/truffle/fund_burner_account.ts
+++ packages/protocol/scripts/truffle/fund_burner_account.ts
@@ -19,78 +19,38 @@ module.exports = async (callback: (error?: any) => number) => {
       string: ['key', 'amount'],
     })
 
-    console.log(argv.key)
-
+    //Use connection to send transactions from local, secure wallet to unsecure address on node
     const web3: Web3 = new Web3('http://127.0.0.1:8545')
-
-    // web3.eth.accounts.privateKeyToAccount(
-    //   '0x3262cbe4bdd55a27ba11ca4674fc91afe0539f850f3074dc06928c5bf9a0e10d'
-    // )
-
-    var burnerAddress: string
-
     const connection = new Connection(web3, new LocalWallet())
+    connection.addAccount(argv.key)
 
-    connection.addAccount('0x3262cbe4bdd55a27ba11ca4674fc91afe0539f850f3074dc06928c5bf9a0e10d')
+    //Get the address of and unlock burner account created in contracts-release.yml
+    var burnerAddress: string
+    const accounts = await connection.getAccounts()
+    burnerAddress = accounts[0]
 
-    // connection.getBalance('0x7F871c887e6a430D3c1F434737F568B07559F9E7').then(console.log)
+    await web3.eth.personal.unlockAccount(burnerAddress, 'A', 0, function (err) {
+      if (err) {
+        console.log('Error accessing burner address!')
+      }
+    })
 
-    // connection.getAccounts().then((e) => (connection.defaultAccount = e[0]))
-
-    // connection.getAccounts().then(console.log)
-    // connection.getAccounts()
-
-    // var burnerAddress: string = await
-    //   connection.getAccounts().then(function (e) {
-    //     const burnerAddress = e[0]
-    //   })
-
-    const prom = await connection.getAccounts()
-
-    burnerAddress = prom[0]
-
-    console.log(burnerAddress)
-
-    await web3.eth.personal.unlockAccount(burnerAddress, 'A', 0)
-
-    // connection.getAccounts().then(console.log)
-
+    //Send transaction from secure local wallet to burner account on node
     await sendTransactionWithPrivateKey(
       web3,
       null,
       '0x3262cbe4bdd55a27ba11ca4674fc91afe0539f850f3074dc06928c5bf9a0e10d',
-      { to: burnerAddress, value: '10000000000000000000', gasPrice: '100000000000' }
+      {
+        to: burnerAddress,
+        value: '10000000000000000000',
+        gasPrice: '100000000000',
+      }
     )
 
-    var bal: string = '0'
-
-    while (bal === '0') {
-      bal = await connection.getBalance(burnerAddress)
-      console.log(bal)
-    }
-
-    // var amount: string = '1000000000000000000'
-
-    // if (argv.amount) {
-    //   amount = argv.amount
-    // }
-
-    // await connection.sendTransaction({
-    //   from: '0x7F871c887e6a430D3c1F434737F568B07559F9E7',
-    //   to: burnerAddress,
-    //   value: '1000000000000000000',
-    //   gasPrice: '100000000000',
-    // })
-
-    // web.eth.personal.defaultAccount = burnerAddress
-    // web.eth.defaultAccount = burnerAddress
-    // connection.getBalance(burnerAddress).then(console.log)
-    // process.env.FROM_BURNER = burnerAddress
-
+    //Save address of burner account in text file so it can be passed to make-release.sh as input
     const fs = require('fs')
-
     fs.writeFileSync('../../burner.txt', burnerAddress)
-    // connection.defaultAccount = '0x7F871c887e6a430D3c1F434737F568B07559F9E7'
+
     callback()
   } catch (error) {
     callback(error)
